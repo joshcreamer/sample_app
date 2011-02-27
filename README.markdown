e98d24fda9200632dcc338df9e6036ea11e83383
joshcreamer <josh.r.creamer@gmail.com>
2011-02-27 14:14:14 -0600

    Added rspec

---
+++ README.markdown
@@ -0,0 +1,3 @@
+#Rails tutorial sample app
+
+This is a sample app after reloading ruby and gems
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6983062511c2de8270679501f0f32e66e7d7c960d60c2b1d73049725c95e8ebb89db159a5c534fa06e6079cc4ef1bf5f6e6e0b738c0826479af03d4ab96fb909'
